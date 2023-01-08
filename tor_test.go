9ec984b68a304238bc3d768fe38a799836682f7e
Corey Prophitt <git@prophitt.me>
2023-01-08 07:19:56 -0800

    Added additional server source to the test

--- tor_test.go
+++ tor_test.go
@@ -14,14 +14,21 @@ func TestExitNodes(t *testing.T) {
 
 	defer svr.Close()
 
+	svr2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "333.333.333.333\n444.444.444.444\n")
+	}))
+
+	defer svr2.Close()
+
 	Sources = map[string][]byte{
-		svr.URL: []byte{},
+		svr.URL:  []byte{},
+		svr2.URL: []byte{},
 	}
 
 	ips := ExitNodes()
 
-	if len(ips) != 2 {
-		t.Fatalf("failed to find two ips; got %d", len(ips))
+	if len(ips) != 4 {
+		t.Fatalf("failed to find four ips; got %d", len(ips))
 	}
 }
 
@@ -32,8 +39,15 @@ func TestIsExitNode(t *testing.T) {
 
 	defer svr.Close()
 
+	svr2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "333.333.333.333\n444.444.444.444\n")
+	}))
+
+	defer svr2.Close()
+
 	Sources = map[string][]byte{
-		svr.URL: []byte{},
+		svr.URL:  []byte{},
+		svr2.URL: []byte{},
 	}
 
 	if exit, err := IsExitNode("111.111.111.111"); err != nil || !exit {
